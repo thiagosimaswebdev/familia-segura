07177ae3596637a16713d0ebe7cb72d82745155d
thiagosimaswebdev <thiagosimas1@gmail.com>
2026-04-23 00:42:03 -0300

    🚀 feat: backend completo com abrigos, famílias, auth, swagger e seed

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,12 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  host:     process.env.DB_HOST,
-  port:     process.env.DB_PORT,
-  user:     process.env.DB_USER,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
